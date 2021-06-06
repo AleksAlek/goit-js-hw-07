ea23e58ef088e680755024e0e8ef366c24c5aea1
Oleksandr Semak <aleks@osemak.kiv.dataart.net>
2021-06-06 20:49:08 +0300

    Resolve task 6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputArea = document.querySelector("input");
+console.log(inputArea);
+const requaredNumber = inputArea.getAttribute("data-length");
+console.log(requaredNumber);
+
+function checkClassName() {
+  if (inputArea.value.length < requaredNumber) {
+    inputArea.classList.add("invalid");
+  } else {
+    inputArea.classList.replace("invalid", "valid");
+  }
+}
+
+inputArea.addEventListener("blur", checkClassName);
+console.log(inputArea);
